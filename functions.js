0b4eb7f33f30933aa633c2b2cad1c84b325625d4
Your Name <you@example.com>
2019-12-03 17:14:20 -0800

    animemes

--- functions.js
+++ functions.js
@@ -18,6 +18,10 @@ exports.commands = function(message, bot) {
 			animeme(message, bot);
 			return;
 		}
+		if (message.content.includes("jojo")) {
+			jojo(message, bot);
+			return;
+		}
 		if (message.content.includes("history_meme")|| message.content.includes("meme")) { 
 			history_meme(message, bot);
 			return;
@@ -59,6 +63,21 @@ history_meme = async (message, bot) => {
     try {
         const { body } = await snekfetch
             .get('https://www.reddit.com/r/historymemes.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        message.channel.send(allowed[randomnumber].data.url)
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
+jojo = async (message, bot) => {
+	const snekfetch = require('snekfetch');
+    try {
+        const { body } = await snekfetch
+            .get('https://www.reddit.com/r/ShitPostCrusaders.json?sort=top&t=week')
             .query({ limit: 800 });
         const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
         if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
@@ -148,6 +167,10 @@ help = function(message) {
       {
         name: ".animeme",
         value: "Sends an animeme. "
+      },
+      {
+        name: ".jojo",
+        value: "Sends an meme from r/shitpostcrusaders. "
       },
       {
         name: ".milestokm (number)",
